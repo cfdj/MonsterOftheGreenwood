5cfb46a0e086c6ac063f6fdb0e753a332baa004e
cfdj <callumderry@gmail.com>
2024-04-16 20:41:03 +0100

    TTS controller quick fix

--- StartMenu.gd
+++ StartMenu.gd
@@ -19,10 +19,10 @@ func _ready():
 	TTs.speak("Monsters of the Greenwood",false)
 	
 func _on_start_game_focus_entered():
-	TTs.speak("Start Game",false)
+	TTs.speak("Start Game",true)
 
 func _on_start_game_pressed():
-	TTs.speak("entering the greenwood",false)
+	TTs.speak("entering the greenwood",true)
 	get_tree().change_scene_to_file("res://levels/Level1.tscn")
 
 
@@ -31,46 +31,46 @@ func _on_tts_enabled_toggled(toggled_on):
 
 
 func _on_tts_enabled_focus_entered():
-	TTs.speak("TTS enabled",false)
+	TTs.speak("TTS enabled",true)
 
 
 func _on_volume_drag_ended(value_changed):
 	TTs.volume = TTSVolume.value;
-	TTs.speak(str(TTs.volume),false)
+	TTs.speak(str(TTs.volume),true)
 
 func _on_volume_focus_entered():
-	TTs.speak("TTS volume",false)
+	TTs.speak("TTS volume",true)
 
 
 func _on_rate_drag_ended(value_changed):
 	TTs.rate = TTSRate.value
-	TTs.speak(str(TTs.rate),false)
+	TTs.speak(str(TTs.rate),true)
 
 
 func _on_voices_focus_entered():
-	TTs.speak("TTS voice",false)
+	TTs.speak("TTS voice",true)
 
 
 func _on_voices_item_focused(index):
-	TTs.speak(str(index),false)
+	TTs.speak(str(index),true)
 
 func _on_voices_item_selected(index):
-	TTs.speak("Voice selected",false)
+	TTs.speak("Voice selected",true)
 	if(index != -1):
 		TTs.voice = index
 
 
 func _on_check_button_toggled(toggled_on):
 	if toggled_on:
-		TTs.speak("Keyboard/Controller enabled",false)
+		TTs.speak("Keyboard/Controller enabled",true)
 		Settings.mouse = !toggled_on
 	else:
-		TTs.speak("Mouse controls enabled",false)
+		TTs.speak("Mouse controls enabled",true)
 		Settings.mouse = !toggled_on
 
 
 func _on_check_button_focus_entered():
-	TTs.speak("Enable keyboard/controller",false)
+	TTs.speak("Enable keyboard/controller",true)
 
 
 func _on_quit_pressed():
@@ -78,15 +78,24 @@ func _on_quit_pressed():
 
 
 func _on_quit_focus_entered():
-	TTs.speak("Quit game",false)
+	TTs.speak("Quit game",true)
 
 
 func _on_contrast_mode_focus_entered():
-	TTs.speak("Enable high contrast",false)
+	TTs.speak("Enable high contrast",true)
 
 func _on_contrast_mode_toggled(toggled_on):
 	Settings.highContrast = toggled_on
 
 
 func _on_rate_focus_entered():
-	TTs.speak("TTs rate",false)
+	TTs.speak("TTs rate",true)
+
+
+func _on_volume_value_changed(value):
+	TTs.volume = value;
+
+
+
+func _on_rate_value_changed(value):
+	TTs.rate = value;
